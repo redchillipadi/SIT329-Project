371739beb33da4e804b22de06d1e679db7460c7e
Adrian Grigo <agrigo2001@yahoo.com.au>
2022-09-12 21:50:46 +1000

    NEWP: Delete old files

--- Quartus/test1/obj/default/getting_started.d
+++
@@ -1,8 +0,0 @@
-obj/default/getting_started.o: getting_started.c BSP/system.h \
- BSP/linker.h BSP//drivers/inc/altera_avalon_pio_regs.h BSP//HAL/inc/io.h \
- BSP//HAL/inc/alt_types.h
-BSP/system.h:
-BSP/linker.h:
-BSP//drivers/inc/altera_avalon_pio_regs.h:
-BSP//HAL/inc/io.h:
-BSP//HAL/inc/alt_types.h:
