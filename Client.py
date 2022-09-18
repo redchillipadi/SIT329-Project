163fe04d26b194ac03bc0fc7c5546053191a9c8f
Nadav Fedida <nadavz_@hotmail.com>
2022-09-18 13:42:51 +1000

    ammended Program.yp file to send time correctly

--- Client.py
+++ Client.py
@@ -11,16 +11,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(str(msg.payload))
     message = msg.payload.decode()
-    # if "sitting" in message:
-    #     print("User is at low position")
 
-    # if msg.payload == "crossbar":
-    #     print("User is at mid position")
-
-    # if msg.payload == "standing":
-    #     print("User is at high position")
-
-# connent to publisher
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
